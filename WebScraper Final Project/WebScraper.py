7a5d50691d2790ead67dc1e492145225019d57e8
Chad-Saltzman <darealcsalt@gmail.com>
2022-04-25 21:17:16 -0700

    Update WebScraper.py

--- WebScraper Final Project/WebScraper.py
+++ WebScraper Final Project/WebScraper.py
@@ -5,6 +5,7 @@ def setupWebDriver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(options = options)
 
     return driver
